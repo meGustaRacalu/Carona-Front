c8f3d12738a79b7ee1e66d0b603cb46d0e84f7a3
Carla <carla.jemaitis@gmail.com>
2025-02-03 19:04:58 -0300

    teste3

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,50 +1,26 @@
+import React from 'react';
+
 function Home() {
-    return (
-        <>
-            <div
-                className="flex-grow justify-center"
-                style={{ backgroundColor: '#00557f' }}>
-                <div style={{
-                    width: "100vw",
-                    display: "flex",
-                    justifyContent: "center"
-                }}>
-                    <div style={{
-                        width: "80vw",
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "center"
-                    }}>
-                        <div style={{
-                            width: "50%",
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center"
-                        }}>
-                            <img
-                                src="https://imgur.com/OiHVAke.png"
-                                alt="Logo pet ride"
-                                width="280px"
-                            />
-                            <h3>Seu pet merece estar ao seu lado <br />aonde você for </h3>
-                        </div>
-                        <div style={{
-                            width: "50%",
-                            display: "flex",
-                            alignItems: "center" // Alinhar verticalmente a imagem maior
-                        }}>
-                            <img
-                                src="https://imgur.com/HVRad2C.png"
-                                alt="Imagem cachorro + motorista"
-                                width="600px"
-                                style={{ borderRadius: '30px' }}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <div 
+      className="flex flex-col justify-center items-center h-screen" 
+      style={{ 
+        backgroundImage: `url('https://imgur.com/43vYaNA.png')`, 
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center', 
+        backgroundRepeat: 'no-repeat' 
+      }}
+    >
+      <div className="text-center text-white">
+        <img 
+          src="https://imgur.com/OiHVAke.png" 
+          alt="Logo pet ride" 
+          width="150px" 
+        />
+        <h3 className="mt-4">Seu pet merece estar ao seu lado <br />aonde você for</h3>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
