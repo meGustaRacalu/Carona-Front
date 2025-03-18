f2a1acb00fbffb0543bdb71c04880a72c65d91a9
Carla <carla.jemaitis@gmail.com>
2025-03-18 17:36:10 -0300

    Imagem da page perfil alterada

--- src/pages/perfil/perfil.tsx
+++ src/pages/perfil/perfil.tsx
@@ -19,18 +19,19 @@ function Perfil() {
         <div className='container mx-auto m-4 rounded-2xl overflow-hidden'>
             <img 
                 className='w-full h-72 object-cover border-b-8 border-white' 
-                src="https://img.freepik.com/fotos-premium/close-up-de-um-cao-pastor-australiano-com-um-fundo-azul-pastel-foto-de-moda-de-cachorro-ia-generativa_796128-1443.jpg?semt=ais_hybrid" 
+                style={{ height: "300px" }} 
+                src="https://imgur.com/yJpjaeL.png" 
                 alt="Capa do Perfil" 
             />
 
             <img 
-                className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' 
+                className='rounded-full w-56 mx-auto mt-[-2rem] top-14 border-8 border-white relative z-10' 
                 src={usuario.foto} 
                 alt={`Foto de perfil de ${usuario.nome}`} 
             />
 
             <div 
-                className="relative mt-[-6rem] h-72 flex flex-col 
+                className="relative mt-[-1rem] h-62 flex flex-col 
                     bg-[#003f5c] text-white text-2xl items-center justify-center"
             >
                 <p>Nome: {usuario.nome}</p>
